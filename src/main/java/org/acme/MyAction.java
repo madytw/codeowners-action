9472657f2a9e8eaf7197fdf86d188275833b0d65
Raman Hryneuski <Raman.Hryneuski@ukg.com>
2023-03-06 19:54:21 +0300

    test

--- src/main/java/org/acme/MyAction.java
+++ src/main/java/org/acme/MyAction.java
@@ -1,6 +1,8 @@
 package org.acme;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 
 import org.kohsuke.github.GHEventPayload;
 import org.kohsuke.github.GitHub;
@@ -13,19 +15,25 @@ public class MyAction {
 
     @Action
     void onPushHandler(@Push GHEventPayload.Push pushPayload, GitHub gitHub, Commands commands) {
+        System.out.println("Commits size: " + Optional.ofNullable(pushPayload.getCommits()).map(List::size).orElse(0));
+        System.out.println("Pusher email: " + Optional.ofNullable(pushPayload.getPusher()).map(GHEventPayload.Push.Pusher::getEmail).orElse("UNKNOWN"));
         var files = new HashSet<>();
         for (var commit : pushPayload.getCommits()) {
+            System.out.println("Commit url: " + commit.getUrl());
             try {
                 files.addAll(commit.getAdded());
             } catch (Exception ex) {
+                System.out.println(ex.getMessage());
             }
             try {
                 files.addAll(commit.getModified());
             } catch (Exception ex) {
+                System.out.println(ex.getMessage());
             }
             try {
                 files.addAll(commit.getRemoved());
             } catch (Exception ex) {
+                System.out.println(ex.getMessage());
             }
 
         }
