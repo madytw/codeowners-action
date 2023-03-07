cbb736afeaff67573be8778026cfa5f700528597
Raman Hryneuski <Raman.Hryneuski@ukg.com>
2023-03-07 09:51:43 +0300

    test

--- src/main/java/org/acme/MyAction.java
+++ src/main/java/org/acme/MyAction.java
@@ -1,9 +1,11 @@
 package org.acme;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
+import org.kohsuke.github.GHCommit;
 import org.kohsuke.github.GHEventPayload;
 import org.kohsuke.github.GitHub;
 
@@ -14,28 +16,16 @@ import io.quarkiverse.githubapp.event.Push;
 public class MyAction {
 
     @Action
-    void onPushHandler(@Push GHEventPayload.Push pushPayload, GitHub gitHub, Commands commands) {
+    void onPushHandler(@Push GHEventPayload.Push pushPayload, GitHub gitHub, Commands commands) throws IOException {
         System.out.println("Commits size: " + Optional.ofNullable(pushPayload.getCommits()).map(List::size).orElse(0));
         System.out.println("Pusher email: " + Optional.ofNullable(pushPayload.getPusher()).map(GHEventPayload.Push.Pusher::getEmail).orElse("UNKNOWN"));
         var files = new HashSet<>();
+
         for (var commit : pushPayload.getCommits()) {
             System.out.println("Commit url: " + commit.getUrl());
-            try {
-                files.addAll(commit.getAdded());
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
-            try {
-                files.addAll(commit.getModified());
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
-            try {
-                files.addAll(commit.getRemoved());
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
 
+            GHCommit commit1 = pushPayload.getRepository().getCommit(commit.getSha());
+            files.addAll(commit1.getFiles());
         }
 
         commands.appendJobSummary("Files size: " + files.size());
