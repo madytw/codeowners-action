92327f232666b751a1f189da8d88409556c531b9
Raman Hryneuski <Raman.Hryneuski@ukg.com>
2023-03-07 09:58:07 +0300

    test

--- src/main/java/org/acme/MyAction.java
+++ src/main/java/org/acme/MyAction.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.kohsuke.github.GHCommit;
 import org.kohsuke.github.GHEventPayload;
@@ -20,14 +21,33 @@ public class MyAction {
         System.out.println("Commits size: " + Optional.ofNullable(pushPayload.getCommits()).map(List::size).orElse(0));
         System.out.println("Pusher email: " + Optional.ofNullable(pushPayload.getPusher()).map(GHEventPayload.Push.Pusher::getEmail).orElse("UNKNOWN"));
         var files = new HashSet<>();
+        var files1 = new HashSet<>();
 
         for (var commit : pushPayload.getCommits()) {
             System.out.println("Commit url: " + commit.getUrl());
+            try {
+                files.addAll(commit.getAdded());
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+            try {
+                files.addAll(commit.getModified());
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+            try {
+                files.addAll(commit.getRemoved());
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+
 
             GHCommit commit1 = pushPayload.getRepository().getCommit(commit.getSha());
-            files.addAll(commit1.getFiles());
+            files1.addAll(commit1.getFiles().stream().map(GHCommit.File::getFileName).collect(Collectors.toList()));
         }
 
+        commands.appendJobSummary("Files1 size: " + files1.size());
+        files.forEach(file -> commands.appendJobSummary("File1: " + file));
         commands.appendJobSummary("Files size: " + files.size());
         files.forEach(file -> commands.appendJobSummary("File: " + file));
     }
